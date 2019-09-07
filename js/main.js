9a3e5a0be0f32aa5059f4868e2c1c116a5ffe73b
DanielPerezArmeria <danoso@hotmail.com>
2019-09-07 11:58:51 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+﻿var button = d3.select(".btn")
+var clearButton = d3.select("#clear-btn")
+var ufoTable = d3.select("thead")
+
+
+button.on("click", function (i) {
+	var inputElement = d3.select("#datetime");
+	var inputValue = inputElement.property("value");
+
+	var day = moment(inputValue).format("D/M/YYYY")
+
+	data.forEach((v) => {
+		var row = ufoTable.append("tr").attr('class','tRow');
+		if (v.datetime === day) {
+			row.append("td").text(v.datetime);
+			row.append("td").text(v.city)
+			row.append("td").text(v.state)
+			row.append("td").text(v.country)
+			row.append("td").text(v.shape)
+			row.append("td").text(v.durationMinutes)
+			row.append("td").text(v.comments)
+		}
+	});
+
+});
+
+clearButton.on("click", function () {
+	d3.selectAll(".tRow").remove()
+});
